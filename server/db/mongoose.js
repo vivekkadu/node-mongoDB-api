5da9708da639fb09799e714787cd525a748ff7d1
vivekkadu <kaduvivek8@gmail.com>
2018-06-03 23:48:58 +0530

    Addeed to g=heroku using Mlab 3

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -2,7 +2,7 @@ var mongoose = require('mongoose');
 
 mongoose.Promise = global.Promise;
 
-const url = 'mongodb://<vivek>:<vivek97>@ds147450.mlab.com:47450/vivektodos';
+const url = 'mongodb://vivek:vivek97@ds147450.mlab.com:47450/vivektodos';
 
 
 mongoose.connect(url);
