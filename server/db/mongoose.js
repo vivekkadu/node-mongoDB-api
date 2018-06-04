45441335e279709d0fbe03e91c61bfdb327da7bf
vivekkadu <kaduvivek8@gmail.com>
2018-06-04 10:22:55 +0530

    Added Delete Route for todos

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -2,9 +2,6 @@ var mongoose = require('mongoose');
 
 mongoose.Promise = global.Promise;
 
-const url = 'mongodb://vivek:vivek97@ds147450.mlab.com:47450/vivektodos';
-
-
-mongoose.connect(url);
+mongoose.connect('mongodb://vivek:vivek97@ds147450.mlab.com:47450/vivektodos' && 'mongodb://localhost:27017/TodoApp');
 
 module.exports = {mongoose}
--- server/server.js
+++ server/server.js
@@ -60,6 +60,29 @@ app.get('/todos/:id', (req, res) => {
 });
 
 
+//Delete Todos 
+
+app.delete('/todos/:id', (req, res) => {
+
+var id = req.params.id;
+
+if(!ObjectID.isValid(id)){
+    return res.status(404).send();
+}
+
+Todo.findByIdAndRemove(id).then((todo) => {
+    if(!todo){
+        return res.status(404).send();
+    }
+    res.send(todo);
+}, (err) => {
+   res.status(400).send(err);
+});
+
+});
+
+
+
 app.listen(port, () => {
     console.log('STARTED ON PORT', port);
 })
