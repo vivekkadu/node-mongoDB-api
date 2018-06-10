813f7f42b9a6ec599ac8f17c053410ab84d3f2f7
vivekkadu <kaduvivek8@gmail.com>
2018-06-10 20:18:09 +0530

    add users/login

--- server/models/user.js
+++ server/models/user.js
@@ -74,6 +74,30 @@ UserSchema.statics.findByToken = function(token) {
 }
 
 
+UserSchema.statics.findByCredentials = function (email, password) {
+    var User = this;
+  
+    return User.findOne({email}).then((user) => {
+      if (!user) {
+        return Promise.reject();
+      }
+  
+      return new Promise((resolve, reject) => {
+        // Use bcrypt.compare to compare password and user.password
+        bcrypt.compare(password, user.password, (err, res) => {
+          if (res) {
+            resolve(user);
+          } else {
+            reject();
+          }
+        });
+      });
+    });
+  };
+
+  
+
+
 //this will run before an activity on Schema in  this case it is save
 
 UserSchema.pre('save' , function (next) {
